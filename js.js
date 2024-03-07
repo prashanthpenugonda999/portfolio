31c8ea644895f34935ef37728ecd004790f7fa97
prashanth <prashanthprince929@gmail.com>
2024-03-07 20:37:32 +0530

    resume

--- js.js
+++ js.js
@@ -1,1 +1,24 @@
 
+console.log("hi")
+let resume_skills=document.getElementById("skill")
+let resume_education=document.getElementById("ed")
+let link=document.getElementById("link")
+let link2=document.getElementById("link2")
+console.log(resume_education)
+console.log(resume_skills)
+
+function Skills(){
+    resume_skills.classList.toggle("display")
+    link.style.color="red"
+    link2.style.color="white"
+    resume_education.classList.add("display")
+    
+   
+ }
+ function Education(){
+    resume_education.classList.toggle("display")
+    link2.style.color="red"
+    link.style.color="white"
+    resume_skills.classList.add("display")
+    
+}
